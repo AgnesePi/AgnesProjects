286c4c652933d0c31e5f3216176cac5cec562f30
AgnesePi <agnese.pinfari@gmail.com>
2024-05-14 17:22:28 +0200

    fixed cards spacing, fixed text spacing in card

--- arts.js
+++ arts.js
@@ -10,17 +10,3 @@ bottoneBack.addEventListener('click', () => {
 bottoneForward.addEventListener('click', () => {
     itemlist.scrollLeft += (itemWitdh + padding)
 })
-// ok grazie amore mio... continua a non funzion però,  giusto?
-// ma no aspe vai su prozzap
-// minchia YAY
-// allora da questa cosa devi capire questo
-// stavi facendo bottoneBack.addEventListener, giusto? Ecco, vuol dire che stavi applicando una funzione a bottoneBack
-// il punto è: cos'era prima bottoneBack? Nulla. Hai solamente preso l'elemento con document.getElementById
-// ma il punto è che non l'hai assegnato a nulla. Quindi se vuoi usare qualcosa, devi avere prima la variabile
-// poi alla variabile cosa assegni? document.getElementById
-// ohhh... chiaro...a posto... comunque sì, devi ancora capire bene le funzioni, ma ci sta, sono molto difficili all'inizio
-// dai vado a lavurà amore
-
-// bravo.. grazie mille cucciolone!! ti amo!!!!  io ti amo! se tieni aperta la condivisione se aggiungi qualcosa ogni tanto ci dò un occhio!
-// va bene patatone!! sarà fatto <3
-// buona impaginazione grazie cucciolo! buon lavoro!!
