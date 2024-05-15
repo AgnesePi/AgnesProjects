5a30ee897e7cb643117baaa443f84b8cf353293d
AgnesePi <agnese.pinfari@gmail.com>
2024-05-15 11:48:32 +0200

    fixed second slideshow

--- arts.js
+++ arts.js
@@ -11,3 +11,15 @@ bottoneForward.addEventListener('click', () => {
     itemlist.scrollLeft += (itemWitdh + padding)
 })
 
+let slideCorrente = 1;
+
+let slides = document.getElementsByClassName('slide')
+let punti = document.getElementsByClassName('punto')
+
+function setSlide(n) {
+    slides[slideCorrente].style.display = "none";
+    slides[n].style.display = "block";
+    slideCorrente = n
+}
+
+setSlide(0);
