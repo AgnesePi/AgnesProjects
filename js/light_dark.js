130b666ea9d545490286b2331cb93f1fd5b32de6
AgnesePi <agnese.pinfari@gmail.com>
2024-05-20 22:52:35 +0200

    changed some bg colors + color transition

--- js/light_dark.js
+++ js/light_dark.js
@@ -1,5 +1,4 @@
 changeMode = document.getElementById('changeMode')
-link = document.getElementsByName('a')
 
 changeMode.addEventListener('click', function() {
     document.body.classList.toggle('light');
