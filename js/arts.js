c185d9ce7d070d186974a661809a232d8d856a5a
AgnesePi <agnese.pinfari@gmail.com>
2024-05-21 09:43:19 +0200

    added local storage

--- js/arts.js
+++ js/arts.js
@@ -18,7 +18,9 @@ let punti = document.getElementsByClassName('punto')
 
 function setSlide(n) {
     slides[slideCorrente].style.display = "none";
+    punti[slideCorrente].style.backgroundColor = 'rgb(222, 222, 222)'
     slides[n].style.display = "block";
+    punti[n].style.backgroundColor = 'rgb(42, 142, 42)';
     slideCorrente = n
 }
 
--- js/light_dark.js
+++ js/light_dark.js
@@ -1,5 +1,18 @@
-changeMode = document.getElementById('changeMode')
 
-changeMode.addEventListener('click', function() {
-    document.body.classList.toggle('light');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const temaSalvato = localStorage.getItem('tema');
+
+    if (temaSalvato === 'light') {
+        document.body.classList.add('light');
+    }
+
+    document.getElementById('changeMode').addEventListener('click', function() {
+        document.body.classList.toggle('light');
+        if (document.body.classList.contains('light')) {
+            localStorage.setItem('tema', 'light');
+        } else {
+            localStorage.removeItem('tema');
+        }
+    });
 });
