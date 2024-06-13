0fd1b661522e8ee30123ccd77bb301bbb37cf57c
AgnesePi <agnese.pinfari@gmail.com>
2024-06-13 11:40:51 +0200

    added comments

--- js/light_dark.js
+++ js/light_dark.js
@@ -1,31 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var temaSalvato = localStorage.getItem('tema');
+    let temaSalvato = localStorage.getItem('tema');
+    // caricata la pagina, ricerca dell'item 'tema' in local storage che viene assegnato alla variabile 'tema salvato'
 
     if (temaSalvato === 'light') {
         document.body.classList.add('light');
     }
+    // se il tema 'light' è già presente, viene assegnata la classe css 'light' al body del documento
+    // permette il caricamento del tema scelto anche ad un nuovo caricamento della pagina o a un passaggio tra pagine
 
     document.getElementById('changeMode').addEventListener('click', function() {
         document.body.classList.toggle('light');
+        // al click del div 'changeMode', viene svolta la funzione 'toggle' della classe css 'light', che viene 
+        // applicata al body. 
         if (document.body.classList.contains('light')) {
             localStorage.setItem('tema', 'light');
         } else {
             localStorage.removeItem('tema');
         }
+        // se la classe light è stata assegnata, la costante 'light' viene assegnata alla costante 'tema', che viene
+        // salvata in questo modo nel local storage, altrimenti viene rimossa. 
+
+        // questo fa in modo che al successivo caricamento, tema avrà assegnata o non assegnata a sé la classe 'ligt'
+        // salvata nel local storage, quindi potrà essere ricaricata la modalità prescelta, chiara o scura.
     });
 });
-
-// al caricamento della pagina, viene ricercato all'interno del local storage la costante "tema". Se questa è già 
-// presente, impostata come "light", allora viene aggiunta la classe "light", su ogni classe nel css che ne 
-// è provvista. Questo permette ad ogni caricamento di una pagina (DOMContentLoaded), di avere già caricato il tema
-// scelto in precedenza. 
-// 
-// Al click del div chiamato "changeMode", viene svolta la funzione toggle (attiva se disattivata, e disattiva
-// se attivata) sulla classe chiamata "light". Ora, se nella lista di classi quindi assegnate al body è inclusa
-// la classe "light", perché è stato premuto il pulsante, allora, questa informazione viene salvata all'interno 
-// del local storage, precisamente nella variabile "tema". Altrimenti, se la classe "light" non è presente, viene
-// rimosso l'item "tema". 
-// 
-// al successivo caricamento, quindi al rientro nel primo if, "tema" avrà quindi assegnata o non
-// assegnata a sé la classe "light", e questo if farà in modo di applicare o meno lo stile scelto prima del
-// ricaricamento alla pagina stessa.
